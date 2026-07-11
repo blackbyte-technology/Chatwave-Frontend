70a15577490de696a53de6a8fe6f666d6ca36c81
rajdoescode <rajpatel01817@gmail.com>
2026-07-12 02:03:35 +0530

    feat: add PageContent component with DOMPurify for secure HTML rendering

--- src/components/landing/PageContent.tsx
+++ src/components/landing/PageContent.tsx
@@ -1,16 +1,22 @@
 "use client";
 
 import React from "react";
+import DOMPurify from "dompurify";
 
 interface PageContentProps {
   content: string;
 }
 
 const PageContent: React.FC<PageContentProps> = ({ content }) => {
+  const sanitizedContent = DOMPurify.sanitize(content, {
+    ALLOWED_TAGS: ['h1','h2','h3','h4','h5','h6','p','a','ul','ol','li','strong','em','br','img','div','span','table','thead','tbody','tr','th','td','blockquote','pre','code','hr','iframe','video','source'],
+    ALLOWED_ATTR: ['href','src','alt','class','style','target','rel','width','height','id','frameborder','allowfullscreen','allow'],
+  });
+
   return (
     <div 
       className="dynamic-content max-w-none"
-      dangerouslySetInnerHTML={{ __html: content }}
+      dangerouslySetInnerHTML={{ __html: sanitizedContent }}
     />
   );
 };
